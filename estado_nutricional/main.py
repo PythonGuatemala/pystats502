68d3dcbfddd17a65bf08800a139e46a91495d7d3
Python Guatemala <35159432+pythongt@users.noreply.github.com>
2018-01-07 16:19:10 -0600

    main initial commit

---
+++ estado_nutricional/main.py
@@ -0,0 +1,14 @@
+"""Este codigo toma 4 variables: 
+- sexo
+- fecha de nacimiento
+- peso
+- talla
+
+y da como resultado los siguientes indices:
+- peso para edad
+- talla para edad
+- peso-talla
+
+en base a las tablas de referencia:
+http://www.who.int/childgrowth/standards/en/
+"""
